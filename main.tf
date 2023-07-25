25cf789562364b940efc8f5e5cdafe6f0007c025
hvad <david.hannequin@gmail.com>
2023-07-25 14:38:56 +0200

    Add task definition.

--- main.tf
+++ main.tf
@@ -10,3 +10,32 @@ terraform {
 resource "aws_ecs_cluster" "my_cluster" {
   name = "my-cluster" # Naming the cluster
 }
+
+resource "aws_ecs_task_definition" "my_app" {
+
+  family                   = "myapp"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+
+
+  container_definitions = <<DEFINITION
+     [
+       {
+           "cpu": 256,
+           "image": "nginx:latest",
+           "memory": 512,
+           "name": "myapp",
+           "networkMode": "awsvpc",
+           "portMappings": [
+                 {
+                         "containerPort": 8080,
+                         "hostPort": 8080
+                       }
+               ]
+         }
+     ]
+         DEFINITION
+
+}
